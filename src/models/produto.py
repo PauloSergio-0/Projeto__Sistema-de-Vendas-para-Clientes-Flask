ff46f824812172531f1016e2c349cc140a0d4f5f
Francisco Santos <franciscosantos@moobitech.com.br>
2024-10-24 19:38:12 -0300

    feat: complentando o fluxo de produto

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,8 +1,6 @@
-from http.client import HTTPException
-
 from src.database.sessao import db
 from src.exception.exception import ProdutoImportException, ProdutoExisteException
-
+from src.settings.config import Config
 
 class Produto(db.Model):
     __tablename__ = 'produtos'
@@ -12,19 +10,30 @@ class Produto(db.Model):
     codigo = db.Column(db.Integer, nullable=False)
     categoria = db.Column(db.String(30), nullable=False)
     preco = db.Column(db.Float, nullable=False)
-    ativo = db.Column(db.Boolean, default=True, nullable=False)
+    status = db.Column(db.Integer, default=True, nullable=False)
 
-    def __init__(self, nome: str, codigo: int, categoria: int, preco: float, ativo=True):
+    def __init__(self, nome: str, codigo: int, categoria: str, preco: float, status=Config.ATIVO):
         self.id = None
         self.nome = nome
         self.codigo = codigo
         self.categoria = categoria
         self.preco = preco
-        self.ativo = ativo
-
+        self.status = status
 
 class ProdutoDTO:
     """Classe de acesso aos dados do Produto."""
+
+    def get_descricao_status(self, status: int) -> str:
+        if status == Config.INATIVO:
+            return "Inativo"
+
+        if status == Config.ATIVO:
+            return "Ativo"
+
+        if status == Config.DELETADO:
+            return "Deletado"
+
+        raise ValueError(f"Status não configurado: {status}")
 
     def importar_produto(self, data: dict) -> None:
         self.__validar_importacao_produto(data)
@@ -39,8 +48,79 @@ class ProdutoDTO:
         if 'id' not in data or not data['id']:
             raise ProdutoImportException("Para importar o produto é necessário ter o id do produto.")
 
-        if self.__produto_existe(data['id']):
+        self.__validar_campos_obrigatorio(data)
+
+        if self.__existe_produto_com_id(data['id']):
             raise ProdutoExisteException("Já existe um produto cadastrado para o id informado.")
 
-    def __produto_existe(self, id_produto: int) -> bool:
+        if self.__existe_produto_com_codigo(data['codigo']):
+            raise ProdutoExisteException("Já existe um produto cadastrado para o código informado.")
+
+    def __validar_campos_obrigatorio(self, data: dict) -> None:
+        if 'nome' not in data or not data['nome']:
+            raise ProdutoImportException("Para importar o produto é necessário ter o nome do produto.")
+
+        if 'codigo' not in data or not data['codigo']:
+            raise ProdutoImportException("Para importar o produto é necessário ter o código do produto.")
+
+        if 'categoria' not in data or not data['categoria']:
+            raise ProdutoImportException("Para importar o produto é necessário ter a categoria do produto.")
+
+        if 'preco' not in data or not data['preco']:
+            raise ProdutoImportException("Para importar o produto é necessário ter o preço do produto.")
+
+    def __existe_produto_com_id(self, id_produto: int) -> bool:
         return Produto.query.filter_by(id=id_produto).first() is not None
+
+    def __existe_produto_com_codigo(self, codigo_produto: int) -> bool:
+        return Produto.query.filter_by(codigo=codigo_produto).first() is not None
+
+    def listar_produtos(self):
+        produtos = Produto.query.all()
+
+        resultado = [{
+            'id': produto.id,
+            'nome': produto.nome,
+            'codigo': produto.codigo,
+            'categoria': produto.categoria,
+            'preco': produto.preco,
+            'status': self.get_descricao_status(produto.status),
+            'status_code': produto.status
+        } for produto in produtos
+        ]
+
+        return resultado
+
+    def cadastar_produto(self, data: dict):
+        self.__validar_campos_obrigatorio(data)
+
+        if self.__existe_produto_com_codigo(data['codigo']):
+            raise ProdutoExisteException("Já existe um produto cadastrado para o código informado.")
+
+        produto = Produto(data['nome'], data['codigo'], data['categoria'], data['preco'])
+
+        db.session.add(produto)
+        db.session.commit()
+
+        return {
+            'id': produto.id,
+            'nome': produto.nome,
+            'codigo': produto.codigo,
+            'categoria': produto.categoria,
+            'preco': produto.preco,
+            'status': self.get_descricao_status(produto.status)
+        }
+
+    def inativar_produto(self, id_produto: int):
+        produto = Produto.query.filter_by(id=id_produto).first()
+        produto.status = Config.INATIVO
+
+        db.session.add(produto)
+        db.session.commit()
+
+    def excluir_produto(self, id_produto: int):
+        produto = Produto.query.filter_by(id=id_produto).first()
+        produto.status = Config.DELETADO
+
+        db.session.add(produto)
+        db.session.commit()
--- src/routes/produto.py
+++ src/routes/produto.py
@@ -10,20 +10,88 @@ from src.models.produto import Produto, ProdutoDTO
 
 def register_routes_produto(app):
 	@app.route('/import_data/produto', methods=['POST'])
-	def registro_produto():
+	def importar_produto():
 		try:
 			data = request.get_json()
 			ProdutoDTO().importar_produto(data)
 
 			return jsonify({
-				"code": 201,
-				"menssage": "adicionado com sucesso"
-			}), 201
+				"code": 200,
+				"menssagem": "Produto importado com sucesso"
+			}), 200
 		except (ProdutoImportException, ProdutoExisteException) as e:
 			return jsonify({
 				"code": 406,
 				"error": str(e)
 			}), 406
+		except Exception as e:
+			return jsonify({
+				"code": 500,
+				"error": "Desculpe-me, ocorreu um erro inesperado."
+			}), 500
+
+	@app.route('/listar/produto', methods=['GET'])
+	def listar_produtos():
+		try:
+			produtos = ProdutoDTO().listar_produtos()
+
+			return jsonify({
+				"code": 200,
+				"produtos": produtos
+			}), 200
+		except Exception as e:
+			return jsonify({
+				"code": 500,
+				"error": f"Desculpe-me, ocorreu um erro inesperado. {str(e)}"
+			}), 500
+
+	@app.route('/cadastrar/produto/', methods=['POST'])
+	def cadastar_produto():
+		try:
+			data = request.get_json()
+			produto = ProdutoDTO().cadastar_produto(data)
+
+			return jsonify({
+				"code": 201,
+				"produto": produto
+			}), 201
+		except ProdutoExisteException as e:
+			return jsonify({
+				"code": 406,
+				"error": str(e)
+			}), 406
+		except Exception as e:
+			return jsonify({
+				"code": 500,
+				"error": "Desculpe-me, ocorreu um erro inesperado."
+			}), 500
+
+	@app.route('/inativar/produto', methods=["POST"])
+	def inativar_produto():
+		try:
+			data = request.get_json()
+			ProdutoDTO().inativar_produto(data['id'])
+
+			return jsonify({
+				"code": 200,
+				"menssagem": "Produto inativado com sucesso!"
+			}), 200
+		except Exception as e:
+			return jsonify({
+				"code": 500,
+				"error": "Desculpe-me, ocorreu um erro inesperado."
+			}), 500
+
+	@app.route('/excluir/produto/', methods=["POST"])
+	def excluir_produto():
+		try:
+			data = request.get_json()
+			ProdutoDTO().excluir_produto(data['id'])
+
+			return jsonify({
+				"code": 200,
+				"menssagem": "Produto deletado com sucesso!"
+			}), 200
 		except Exception as e:
 			return jsonify({
 				"code": 500,
