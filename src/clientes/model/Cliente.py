b19b3d3359728ec3761aea822cffdae1959d714c
Thayanne Oliveira <jobsthay@gmail.com>
2024-11-07 16:34:02 -0300

    att

--- src/clientes/model/Cliente.py
+++ src/clientes/model/Cliente.py
@@ -8,11 +8,11 @@ class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(20), nullable=False)
     endereco = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(20), nullable=False)
+    contato = db.Column(db.String(20), nullable=False)
     status = db.Column(Enum(Status), default=Status.ATIVO.value)
 
-    def __init__(self, nome, endereco, email, status=Status.ATIVO.value):
+    def __init__(self, nome, endereco, contato, status=Status.ATIVO.value):
         self.nome = nome
         self.endereco = endereco
-        self.email = email
+        self.contato = contato
         self.status = status
