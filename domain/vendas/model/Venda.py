94bb2199b107a91e64c46ea1919ee22ed5040e1f
Jonas Santos <jonaskest@hotmail.com>
2024-11-06 21:27:37 -0300

    update model

--- domain/vendas/model/Venda.py
+++ domain/vendas/model/Venda.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from database.sessao import db
 from settings.config import Config
 
@@ -8,10 +9,12 @@ class Venda(db.Model):
     produto_id = db.Column(db.Integer, nullable=False)
     cliente_id = db.Column(db.Integer, nullable=False)
     quantidade = db.Column(db.Integer, nullable=False)
+    data_venda = db.Column(db.DateTime, nullable=False)
     preco_total = db.Column(db.Float, nullable=False)
 
-    def __init__(self, produto_id, cliente_id, quantidade, preco_total):
+    def __init__(self, produto_id, cliente_id, quantidade, data_venda, preco_total):
         self.produto_id = produto_id
         self.cliente_id = cliente_id
         self.quantidade = quantidade
         self.preco_total = preco_total
+        self.data_venda = data_venda
